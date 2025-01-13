98904c2b6edfd81f6a5f1267ac9f9590f7b12910
alejandroesoto <alejandroesoto@gmail.com>
2025-01-12 23:29:18 -0600

    Lesson 8 - Data Fetching Data Refetching Memoized Handlers

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,41 @@ function App() {
   // new state variable named isLoading with a default value true
   const [isLoading, setIsLoading] = useState(true);
 
-  // define the useEffect React hook with an empty dependency list 
-  useEffect(() => { 
-   const fetchData = new Promise((resolve, reject) => { 
-   // mimic a loading delay with setTimeout 
-   setTimeout(() => { 
-     resolve(
-      { data: { todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [] } }
-     ); 
-    }, 2000); 
-  }); 
- 
-  fetchData 
-    .then((result) => { 
-      // use the state setter to update the list 
-      setTodoList(result.data.todoList); 
-      setIsLoading(false);
-    })
-    .catch((error) => { 
-      console.error("An error occurred:", error); 
-      setIsLoading(false);
-    }); 
-  }, []); // empty dependency list
+  // create a new async function fetchData 
+  const fetchData = async () => { 
+    const options = { 
+      method: 'GET', 
+      headers: { 
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`, }
+    }; 
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`; 
+    
+    try { 
+      const response = await fetch(url, options); 
+      if (!response.ok) { 
+        throw new Error(`Error: ${response.status}`); 
+      } 
+      const data = await response.json(); 
 
+      // Transform the data to match the todo schema 
+      const todos = data.records.map(record => ({ 
+       title: record.fields.title,
+       id: record.id
+      }));
+
+      setTodoList(data.records.map(record => ({ id: record.id, ...record.fields }))); 
+      setIsLoading(false); 
+    } catch (error) { 
+      console.error("An error occurred:", error); 
+      setIsLoading(false); 
+    } 
+  }; 
+  
+  // replace the contents of the first useEffect with a call to fetchData 
+  useEffect(() => { 
+    fetchData(); 
+  }, []); // empty dependency list
+    
   // add an if statement to check that isLoading is false before setting localStorage 
   useEffect(() => { 
     if (!isLoading) { 
